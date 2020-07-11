4fcf4accaa65aee9536c8f4a009158cad5e97c24
jslee <etoileettoi@gmail.com>
2020-07-11 14:52:40 +0900

    방 목록 뿌려주는 Adapter 뼈대 작성

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jicbangcopy_20200711/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jicbangcopy_20200711/MainActivity.kt
@@ -2,8 +2,12 @@ package kr.co.tjoeun.listviewpractice_jicbangcopy_20200711
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.listviewpractice_jicbangcopy_20200711.datas.Room
 
 class MainActivity : BaseActivity() {
+
+    val roomList = ArrayList<Room>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
