098737b397cee794d3968b7724f5ee35cf3fed9e
jslee <etoileettoi@gmail.com>
2020-07-12 10:27:20 +0900

    방 상세 화면 그림 그리기 완료

---
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jicbangcopy_20200711/datas/ViewRoomDetailActivity.kt
@@ -0,0 +1,25 @@
+package kr.co.tjoeun.listviewpractice_jicbangcopy_20200711.datas
+
+import android.content.Context
+import android.os.Bundle
+import kr.co.tjoeun.listviewpractice_jicbangcopy_20200711.BaseActivity
+import kr.co.tjoeun.listviewpractice_jicbangcopy_20200711.R
+
+class ViewRoomDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
